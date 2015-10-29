2e0a37e5af1785cba1b5a48440faa3dd61e273c6
cetleite <cetleite@gmail.com>
2015-10-29 13:50:45 -0200

    Tela dividida corretamente em 1,2,3 ou 4 mapas

--- Android/src/org/droidplanner/android/activities/MultipleActivity.java
+++ Android/src/org/droidplanner/android/activities/MultipleActivity.java
@@ -21,24 +21,9 @@ public class MultipleActivity extends FragmentActivity implements MultipleFragme
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_multiple);
 
+        multipleMapView(4);
 
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        MultipleFragment fragment = new MultipleFragment();
-        fragmentTransaction.add(R.id.multi_layout1, fragment, "1");
-
-
-        MultipleFragment fragment2 = new MultipleFragment();
-        fragmentTransaction.add(R.id.multi_layout2, fragment2, "2");
-
-        MultipleFragment fragment3 = new MultipleFragment();
-        fragmentTransaction.add(R.id.multi_layout3, fragment3, "3");
-
-        fragmentTransaction.commit();
     }
 
     @Override
@@ -71,6 +56,44 @@ public class MultipleActivity extends FragmentActivity implements MultipleFragme
     public void onFragmentInteraction2(Uri uri)
     {
 
+    }
+
+    public void multipleMapView(int num_maps)
+    {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        MultipleFragment fragment1 = new MultipleFragment();
+        MultipleFragment fragment2 = new MultipleFragment();
+        MultipleFragment fragment3 = new MultipleFragment();
+        MultipleFragment fragment4 = new MultipleFragment();
+        switch(num_maps)
+        {
+            case 1:
+                setContentView(R.layout.activity_multiple1);
+                fragmentTransaction.add(R.id.multi_layout1, fragment1, "1");
+                break;
+            case 2:
+                setContentView(R.layout.activity_multiple2);
+                fragmentTransaction.add(R.id.multi_layout1, fragment1, "1");
+                fragmentTransaction.add(R.id.multi_layout2, fragment2, "2");
+                break;
+            case 3:
+                setContentView(R.layout.activity_multiple3);
+                fragmentTransaction.add(R.id.multi_layout1, fragment1, "1");
+                fragmentTransaction.add(R.id.multi_layout2, fragment2, "2");
+                fragmentTransaction.add(R.id.multi_layout3, fragment3, "3");
+
+                break;
+            case 4:
+                setContentView(R.layout.activity_multiple4);
+                fragmentTransaction.add(R.id.multi_layout1, fragment1, "1");
+                fragmentTransaction.add(R.id.multi_layout2, fragment2, "2");
+                fragmentTransaction.add(R.id.multi_layout3, fragment3, "3");
+                fragmentTransaction.add(R.id.multi_layout4, fragment4, "4");
+                break;
+        }
+
+        fragmentTransaction.commit();
     }
 
 
